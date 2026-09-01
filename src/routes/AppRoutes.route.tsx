6dc56764fdb3a7a7556649fd8b0dd1fd1e847d57
Adesh-joshi <adeshjoshi91@gmail.com>
2026-09-01 22:38:15 +0545

    feat(home): advance course in homepage

--- src/routes/AppRoutes.route.tsx
+++ src/routes/AppRoutes.route.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../features/home/pages/HomePage";
+import AdmissionForm from "../features/admission/pages/AdmissionForm";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/admission-form" element={<AdmissionForm />} />
     </Routes>
   );
 };
